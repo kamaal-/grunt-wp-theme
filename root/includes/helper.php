0b6a4db277a2f4578e76018665b56ab0383d0706
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-09 12:53:54 +0400

    fix helper.php
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/includes/helper.php
+++ root/includes/helper.php
@@ -9,7 +9,7 @@ if( !function_exists( '{%= prefix %}_get_theme_version' ) ):
  ** @since {%= version %}
  ** @return string
  **/
-  function {%= prefix %}_get_theme_version() {
+function {%= prefix %}_get_theme_version() {
 
   $theme = wp_get_theme();
   return $theme->get('Version');
