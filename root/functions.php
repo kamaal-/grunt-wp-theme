78d4b4e3f0ffc58bc148bbb96f37dc717eb252e5
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-11 16:36:57 +0400

    fix functions.php
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/functions.php
+++ root/functions.php
@@ -19,3 +19,27 @@
  **/
 require get_template_directory() . '/includes/helpers.php';
 
+
+/**
+ ** Set the content width based on the theme's design and stylesheet.
+ **/
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 640; /* pixels */
+}
+
+if ( ! function_exists( '{%= prefix %}_setup' ) ) :
+/**
+ ** Sets up theme defaults and registers support for various WordPress features.
+ **
+ ** Note that this function is hooked into the after_setup_theme hook, which
+ ** runs before the init hook. The init hook is too late for some features, such
+ ** as indicating support for post thumbnails.
+ ** @package {%= title %}
+ ** @since {%= version %}
+ **/
+function {%= prefix %}_setup(){
+	
+}
+
+endif;
