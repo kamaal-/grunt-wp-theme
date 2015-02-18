9d88103cc5d68394b2cb622573b72131adeb95ac
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-18 17:00:54 +0400

    sidebar
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/sidebar.php
+++ root/sidebar.php
@@ -1,2 +1,15 @@
 <?php
+/**
+ ** The sidebar containing the main widget area.
+ **
+ ** @package {%= title %}
+ ** @since {%= version %}
+ **/
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
 ?>
+
+<aside id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
