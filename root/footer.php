4538f7a508dcaf20633ed0a71168578cad30f5a5
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-18 17:00:14 +0400

    footer code
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/footer.php
+++ root/footer.php
@@ -1,2 +1,14 @@
-<?php 
+<?php
+/**
+ ** The template for displaying the footer.
+ **
+ ** Contains most of the closing tags
+ **
+ ** @package {%= title %}
+ ** @since {%= version %}
+ **/
 ?>
+<?php wp_footer(); ?>
+
+</body>
+</html>
