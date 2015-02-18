91e76c7defcdc8e02a97e1e080fd8d82ff686ac7
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-18 16:48:54 +0400

    index php codes
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/index.php
+++ root/index.php
@@ -1,2 +1,18 @@
 <?php
-?>
+/**
+ ** The main template file.
+ **
+ ** This is the most generic template file in a WordPress theme
+ ** and one of the two required files for a theme (the other being style.css).
+ ** It is usedto display a pa ge when nothing more specific matches a query.
+ ** E.g., it puts together the home page when no home.php file exists.
+ ** Learn more: http://codex.wordpress.org/Template_Hierarchy
+ **
+ ** @package {%= title %}
+ ** @since {%= version %}
+ **/
+
+get_header(); ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
