e24eea872082d3ba291ef057d5e365fe65f77468
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-18 16:59:59 +0400

    header code
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/header.php
+++ root/header.php
@@ -1,2 +1,20 @@
-<?php 
-?>
+<?php
+/**
+ ** The header for {%= title %}.
+ **
+ ** Displays all of the <head> section and everything up till start of the content
+ **
+ ** @package {%= title %}
+ ** @since {%= version %}
+ **/
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
