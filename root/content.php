12f1b44de891a5c8710479efad9245cb87f996b0
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-22 15:38:56 +0400

    content
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/content.php
+++ root/content.php
@@ -1,2 +1,38 @@
 <?php
+/**
+ ** @package {%= title %}
+ ** @since {%= version %}
+ **/
 ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php _s_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Read more %s <span class="meta-nav">&rarr;</span>', '_s' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', '_s' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php _s_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
