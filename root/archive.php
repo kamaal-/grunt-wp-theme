0fdbcf0e8648fd64bc9ff44b844e838312380fad
Kamaal Aboothalib <kamaal@kamaal.me>
2015-02-22 15:42:30 +0400

    archive
    Archive haeder
    
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/archive.php
+++ root/archive.php
@@ -1,2 +1,10 @@
 <?php
-?>
+/**
+ ** The template for displaying archive pages.
+ **
+ ** Learn more: http://codex.wordpress.org/Template_Hierarchy
+ **
+ ** @package {%= title %}
+ ** @since {%= version %}
+ **/
+get_header(); ?>
