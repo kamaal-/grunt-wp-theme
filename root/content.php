2fb6e950270a07ce7ad6165646e31fea25fbcbe9
Kamaal Aboothalib <kamaal@kamaal.me>
2015-03-24 11:15:35 +0400

    error
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- root/content.php
+++ root/content.php
@@ -14,7 +14,7 @@
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php '{%= prefix %}'_posted_on(); ?>
+			<?php {%= prefix %}_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -38,6 +38,6 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php '{%= prefix %}'_entry_footer(); ?>
+		<?php {%= prefix %}_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- root/includes/template-tags.php
+++ root/includes/template-tags.php
@@ -35,6 +35,37 @@ function {%= prefix %}_posted_on() {
 }
 endif;
 
+
+
+
+/**
+ * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
+ */
+function {%= prefix %}_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( '_s_categories' ) ) ) {
+		// Create an array of all the categories that are attached to posts.
+		$all_the_cool_cats = get_categories( array(
+			'fields'     => 'ids',
+			'hide_empty' => 1,
+			// We only need to know if there is more than one category.
+			'number'     => 2,
+		) );
+		// Count the number of categories that are attached to the posts.
+		$all_the_cool_cats = count( $all_the_cool_cats );
+		set_transient( '_s_categories', $all_the_cool_cats );
+	}
+	if ( $all_the_cool_cats > 1 ) {
+		// This blog has more than 1 category so _s_categorized_blog should return true.
+		return true;
+	} else {
+		// This blog has only 1 category so _s_categorized_blog should return false.
+		return false;
+	}
+}
+
+
 if ( ! function_exists( '{%= prefix %}_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.
